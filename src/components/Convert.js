47fcf7750054ba04521749a551943e9f334bdd3f
Kaloyan Krumov <enzerbilen@gmail.com>
2020-07-15 09:09:41 +0300

    complete Convert.js

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -1,20 +1,50 @@
 import React, {useState, useEffect} from "react";
+import axios from 'axios';
 
 
 const Convert = ({text, language}) => {
-    const [texting, setText] = useState('')
-    const [lang, setLang] = useState('')
+    const [translated, setTranslated] = useState('')
+
+    //ползваме debouncedText, за да задържим промяната на текста за
+    //определено време, преди да го подадем на doTranslate()
+    //в Search.js задържахме цялата ф-я, преди да се изпълни
+    const [debouncedText, setDebouncedText] = useState(text)
+
+    useEffect(()=>{
+        const timeoutID = setTimeout(() => {
+            if(!text){
+                return;
+            }
+            setDebouncedText(text)
+
+        }, 1000)
+
+        return () => {
+            clearTimeout(timeoutID)
+        };
+
+    }, [text])
 
 
     useEffect(() => {
-        console.log(language)
-        // setText(text)
-        // setLang(language)
-    }, [text, language])
+        const doTranslate = async () => {
+            const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2', {}, {
+                params: {
+                    q: debouncedText,
+                    target: language.text,
+                    key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM'
+                }
+            })
+            setTranslated(data.data.translations[0].translatedText)
+        }
+            doTranslate()
+
+
+    }, [debouncedText, language])
     return (
         <div>
-            <p>{texting}</p>
-            <p>{lang}</p>
+            <p>{translated}</p>
+            {/*<p>{lang}</p>*/}
         </div>
     )
 }
